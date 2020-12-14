7d5a746681164a745776525b25c59f0dc6c422f2
ArulDavid <le.aruldavid@gmail.com>
2020-12-13 17:28:50 -0800

    Test SparkSQL Rebase

--- src/main/java/org/com/spark/SparkSQLPOC.java
+++ src/main/java/org/com/spark/SparkSQLPOC.java
@@ -1,14 +1,9 @@
 package org.com.spark;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkSQLPOC {
     public static void main(String args[]){
         SparkSession sps = SparkSession.builder().appName("Spark SQL Demo").master("local[*]").getOrCreate();
-        Dataset<Row> peopleSet = sps.read().json("C:\\Development\\spark\\SparkQuickDemo\\SparkQuickDemo\\src\\main\\java\\org\\com\\spark\\people.json");
-        peopleSet.show();
-
     }
 }
