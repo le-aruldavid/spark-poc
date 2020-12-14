4142337727bcf87e3caa7c3b69065fac29336e7e
ArulDavid <le.aruldavid@gmail.com>
2020-12-13 17:28:50 -0800

    Test SparkSQL

--- src/main/java/org/com/spark/SparkSQLPOC.java
+++ src/main/java/org/com/spark/SparkSQLPOC.java
@@ -5,5 +5,6 @@ import org.apache.spark.sql.SparkSession;
 public class SparkSQLPOC {
     public static void main(String args[]){
         SparkSession sps = SparkSession.builder().appName("Spark SQL Demo").master("local[*]").getOrCreate();
+        System.out.println("SparkSQL Test");
     }
 }
